0534b497e2f3d823cb40ae1f65e4ac39348ced1d
unknown <mrkbedoya@gmail.com>
2018-07-05 12:48:39 -0700

    near end day 3

---
+++ IO/Reflection.java
@@ -0,0 +1,24 @@
+import java.lang.reflect.Field;
+
+public class Reflection {
+    public static void main(String[] args) throws Exception{ //rather than doing try catch block "throw" the exception somewhere else
+        //read contents of something at run time.
+        // peak inside a file during run time to do something with it.
+        Class<?> c = Class.forName("Person"); // this turns c into an object of "Person"
+        System.out.println(c.getName());
+
+        //Print fields of Person
+        Field[] fields = c.getDeclaredFields();
+        for (Field f : fields) {
+            System.out.println(f.getName());
+        }
+
+        Person joe = (Person) c.newInstance();
+        System.out.println(joe);
+
+        Field newAge = c.getDeclaredField("age");
+        newAge.setAccessible(true);
+        newAge.set(joe, -5000);
+        System.out.println(joe);
+    }
+}
