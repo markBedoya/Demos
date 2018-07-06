8df22114851fd65089bf41c12ea59034ff346c9b
unknown <mrkbedoya@gmail.com>
2018-07-06 08:36:12 -0700

    fri 7/6

--- IO/Reflection.java
+++ IO/Reflection.java
@@ -4,6 +4,7 @@ public class Reflection {
     public static void main(String[] args) throws Exception{ //rather than doing try catch block "throw" the exception somewhere else
         //read contents of something at run time.
         // peak inside a file during run time to do something with it.
+        // the <?> is a generic type that takes anything. it doesnt know what its going to get.
         Class<?> c = Class.forName("Person"); // this turns c into an object of "Person"
         System.out.println(c.getName());
 
---
+++ Threads/Synchro.java
@@ -0,0 +1,50 @@
+public class Synchro {
+    public static void main (String[] args) {
+        //create and start 2 threads
+        //should print 10,000 total
+        //each thread printing 5000 times
+        
+        Count c = new Count();
+        Runnable job = new TestRunnable(c);
+
+        Thread t1 = new Thread(job);
+        Thread t2 = new Thread(job);
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException ex) {
+            ex.getMessage();
+        }
+
+        System.out.println("Count = " + c.count);
+    }
+}
+
+class Count {
+    int count;
+
+    public synchronized void increment() {
+        count++;
+    }
+}
+
+class TestRunnable implements Runnable {
+    Count c;
+    //Count c = new Count();
+    // not using this "new" here is called "dependancy injection"
+    // this is called a design pattern 
+
+    public TestRunnable (Count c) {
+        this.c = c;
+    }
+
+    public void run() {
+        for (int i = 0; i < 5000; i++) {
+            c.increment();
+        }
+    }
+}
